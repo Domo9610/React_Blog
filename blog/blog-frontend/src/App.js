2921542c94ff67893c28615aaedbe1bf8379ce21
Domo9610 <54881558+Domo9610@users.noreply.github.com>
2021-10-23 16:30:38 +0900

    login add

--- blog/blog-frontend/src/App.js
+++ blog/blog-frontend/src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import React from 'react';
 import { Route } from 'react-router-dom';
 import PostListPage from './pages/PostListPage';
 import LoginPage from './pages/LoginPage';
@@ -7,15 +7,14 @@ import WritePage from './pages/WritePage';
 import PostPage from './pages/PostPage';
 
 const App = () => {
-  return(
+  return (
     <>
-    <Route component = {PostListPage} path={['/@:username', '/']} exact/>
-    <Route component = {LoginPage} path='/login' />
-    <Route component = {RegisterPage} path='/register'/>
-    <Route component = {WritePage} path='/write'/>
-    <Route component = {PostPage} path='/@:username/:PostId'/>
+      <Route component={PostListPage} path={['/@:username', '/']} exact />
+      <Route component={LoginPage} path="/login" />
+      <Route component={RegisterPage} path="/register" />
+      <Route component={WritePage} path="/write" />
+      <Route component={PostPage} path="/@:username/:postId" />
     </>
   );
 };
-
 export default App;
--- blog/blog-frontend/src/pages/PostListPage.js
+++ blog/blog-frontend/src/pages/PostListPage.js
@@ -1,11 +1,13 @@
-import Button from '../components/common/Button'
+import React from 'react';
+import HeaderContainer from '../containers/common/HeaderContainer';
 
 const PostListPage = () => {
-    return (
-    <div>
-        <Button>버튼</Button>        
-    </div>
-    );
+  return (
+    <>
+      <HeaderContainer />
+      <div>안녕하세요.</div>
+    </>
+  );
 };
 
 export default PostListPage;
